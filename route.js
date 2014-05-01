6aba078177479d5b03360e4e47c338d0c5bf0d91
amatellanes <dev.amatellanes@gmail.com>
2014-05-02 00:04:57 +0200

    Initial commit

---
+++ route.js
@@ -0,0 +1,5 @@
+function route(pathname) {
+	console.log("Request to " + pathname);
+}
+
+exports.route = route;
---
+++ server.js
@@ -0,0 +1,19 @@
+var http = require("http");
+var url = require("url");
+
+var iniciar = function() {
+	var onRequest = function(request, response) {
+		var pathname = url.parse(request.url).pathname;
+		
+		console.log("Request to " + pathname + " received.");
+		
+		response.writeHead(200, {"Content-Type": "text/html"});
+		response.write("<h1>Hello World</h1>");
+		response.end();
+	};
+
+	http.createServer(onRequest).listen(8888);
+	console.log("Server started");
+};
+
+exports.iniciar = iniciar
