26207f9920ff36ddc79e62dfd4469df14023688b
amatellanes <dev.amatellanes@gmail.com>
2014-07-07 22:46:16 +0200

    Add new Photo model in Gallery app

--- gallery-express-app/app.js
+++ gallery-express-app/app.js
@@ -14,6 +14,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.set('photos', __dirname + '/public/photos');
 app.set('title', 'Gallery');
 
 app.use(favicon());
