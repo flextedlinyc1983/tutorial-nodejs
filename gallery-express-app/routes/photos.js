07340dac8b1f6e21ac90a35d0c719f17a26b4eb8
amatellanes <dev.amatellanes@gmail.com>
2014-07-13 10:43:19 +0200

    Add upload view

--- gallery-express-app/routes/photos.js
+++ gallery-express-app/routes/photos.js
@@ -1,4 +1,9 @@
-var express = require('express');
+var express = require('express'),
+    Photo = require('../models/Photo'),
+    path = require('path'),
+    join = path.join,
+    fs = require('fs');
+
 var router = express.Router();
 
 var photos = [];
@@ -16,6 +21,34 @@ router.get('/', function (req, res) {
     res.render('photos', {
         photos: photos
     })
+});
+
+/* GET upload photo form */
+router.get('/upload', function (req, res) {
+    res.render('photos/upload', {
+        title: 'Photo upload'
+    });
+});
+
+/* submit photo form */
+router.post('/upload', function (dir) {
+    return function (req, res, next) {
+        var img = req.files.photo.image,
+            name = req.body.photo.name || img.name,
+            path = join(dir, img.name);
+
+        fs.rename(img.path, path, function (err) {
+            if (err) return next(err);
+
+            Photo.create({
+                name: name,
+                path: path
+            }, function (err) {
+                if (err) return next(err);
+                res.redirect('/');
+            });
+        });
+    };
 });
 
 module.exports = router;
