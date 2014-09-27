28eb8cd00320bb9aadc8639d12d4d78cac84b100
amatellanes <dev.amatellanes@gmail.com>
2014-09-27 11:28:24 +0200

    Update colorama script

--- command-line tools/colorama.js
+++ command-line tools/colorama.js
@@ -2,5 +2,25 @@ var ansi = require('ansi');
 
 var cursor = ansi(process.stdout);
 
-cursor.
-    fg.green().write("Hello").fg.reset().write("\n");
+cursor
+    .reset()
+    .write("   ")
+    .bold()
+    .underline()
+    .bg.white()
+    .fg.black()
+    .write("Node.js in Action")
+    .fg.reset()
+    .bg.reset()
+    .resetUnderline()
+    .resetBold()
+    .write("  \n")
+    .fg.green()
+    .write(" by:\n")
+    .fg.cyan()
+    .write("   Mike Cantelon\n")
+    .fg.magenta()
+    .write("   TJ Holowaychuk\n")
+    .fg.yellow()
+    .write("   Nathan Rajlich\n")
+    .reset()
